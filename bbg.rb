858e7ea03832a998963ce2fb856fea1e54f541c1
Mary Cotter <marebearc@gmail.com>
2018-08-01 14:08:41 +0000

    automatically backed up by learn

---
+++ bbg.rb
@@ -0,0 +1,18 @@
+# loop do 
+#   puts "leave"
+#   puts "weave"
+#   sleep(0.5)
+#   break
+# end
+
+# puts "Jumpi"
+
+i = 0 
+
+until i > 4
+ puts "leave"
+ puts "weave"
+ i+=1
+end
+
+puts "jumpi"
---
+++ beer.rb
@@ -0,0 +1,4 @@
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number-1} bottles of beer on the wall.
+"No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
